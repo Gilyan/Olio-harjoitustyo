d3e1df1ab64aff29f582982777bd40e51ae02e60
k8517 <k8517>
2017-03-27 12:44:09 +0300

    minor text fixes

--- Oliogotchi/Oliogotchi/GameView.xaml.cs
+++ Oliogotchi/Oliogotchi/GameView.xaml.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿/* **********************************************************
+Oliogotchin pelisivu.
+
+Toiminta: Pelisivun kautta voi 
+- pelata peliä
+- tänne juttuja lisättävä
+
+Luotu 27.3.2017
+
+Minttu Mäkäläinen K8517
+Kioto Hiirola K8252
+Joona Hautamäki K1647
+@ JAMK 
+********************************************************** */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- Oliogotchi/Oliogotchi/SettingsView.xaml.cs
+++ Oliogotchi/Oliogotchi/SettingsView.xaml.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿/* **********************************************************
+Oliogotchin asetussivu.
+
+Toiminta: Asetuksien kautta voi 
+- vaihtaa olion nimeä
+- poistaa olion
+
+Luotu 27.3.2017
+
+Minttu Mäkäläinen K8517
+Kioto Hiirola K8252
+Joona Hautamäki K1647
+@ JAMK 
+********************************************************** */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
