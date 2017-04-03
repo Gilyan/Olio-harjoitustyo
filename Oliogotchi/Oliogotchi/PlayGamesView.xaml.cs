bf7a4d212a8504659011accf143dc6efa695c086
k8517 <k8517>
2017-04-03 12:39:06 +0300

    muutoksia

--- Oliogotchi/Oliogotchi/PlayGamesView.xaml.cs
+++ Oliogotchi/Oliogotchi/PlayGamesView.xaml.cs
@@ -23,6 +23,7 @@ namespace Oliogotchi
         //{
         //    InitializeComponent();
         //}
+        int kumpiPeli = 0;
         public PlayGamesView(double x, double y)     // Ylikuormitetaan, että saadaan ikkunan paikka oikein
         {
             InitializeComponent();
@@ -33,14 +34,37 @@ namespace Oliogotchi
 
         private void btnPlayStone_Click(object sender, RoutedEventArgs e)
         {
+            kumpiPeli = 1;
             btnGoPlay.Visibility = System.Windows.Visibility.Visible;
             txbGameInfo.Text = "Tänne kivi-sakset-paperi -pelin ohjeet";
         }
 
-        private void btnMainMenu_Click(object sender, RoutedEventArgs e)
+        private void btnPlayBall_Click(object sender, RoutedEventArgs e)
         {
+            kumpiPeli = 2;
             btnGoPlay.Visibility = System.Windows.Visibility.Visible;
             txbGameInfo.Text = "Tänne pallon heitto -pelin ohjeet";
+        }
+
+        private void btnGoPlay_Click(object sender, RoutedEventArgs e)
+        {
+            if (kumpiPeli == 1)      // Kivi-sakset-paperi
+            {
+                double x = this.Left;
+                double y = this.Top;
+                PlayStoneView kiviSaksetPaperi = new PlayStoneView(x, y);
+                kiviSaksetPaperi.Show();
+                this.Close();
+            }
+            else if (kumpiPeli == 2)     // Pallonheitto
+            {
+                double x = this.Left;
+                double y = this.Top;
+                PlayBallView haePallo = new PlayBallView(x, y);
+                haePallo.Show();
+                this.Close();
+            }
+            else;
         }
     }
 }
