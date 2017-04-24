1a13679ca197656f4b8fb7df3c53517941c2c1ed
K1647 <K1647@student.jamk.fi>
2017-04-25 00:03:48 +0300

    kommentointia

--- Oliogotchi/Oliogotchi/PlayBallView.xaml.cs
+++ Oliogotchi/Oliogotchi/PlayBallView.xaml.cs
@@ -37,7 +37,7 @@ namespace Oliogotchi
     /// </summary>
     public partial class PlayBallView : Window
     {
-        public int points = 0;
+        //Pisteiden ja nälän propertyt jotta saadaan helposti vietyä GameViewin oliolle
         public int Pisteet
         {
             get { return points; }
@@ -49,63 +49,39 @@ namespace Oliogotchi
             set { hunger = value; }
         }
         int hunger = 0;
-        Point p = new Point();
-        DispatcherTimer timer = new DispatcherTimer(DispatcherPriority.Render);
+        int points = 0;
+        DispatcherTimer timer = new DispatcherTimer(DispatcherPriority.Render);  //DispatcherPriority.Render siksi, että peli toimisi sulavemmin
         Random rnd = new Random();
         bool keyPressed = true;
-
         private MediaPlayer mediaPlayer = new MediaPlayer();    // Äänisoitin
 
         public PlayBallView(double x, double y)     // Ylikuormitetaan, että saadaan ikkunan paikka oikein
         {
             InitializeComponent();
+            //timeri pelille jotta saadaan asiat liikkumaan
             timer.Tick += new EventHandler(timer_Tick);
             timer.Interval = new TimeSpan(0, 0, 0, 0, 10);
             timer.Start();
             this.Left = x;
             this.Top = y;
-            this.KeyDown += new KeyEventHandler(slime_KeyDown);
+            this.KeyDown += new KeyEventHandler(slime_KeyDown); //näppäimistön kuuntelu peliä varten
 
             btnAgain.Visibility = System.Windows.Visibility.Hidden;     // Uudelleenpelausnappula piiloon toistaiseksi
         }
-        //Drag-and-drop ominaisuus pallolle joka jää pois pelistä
-       /* private void ball_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            timer.Start();
-            ball.CaptureMouse();
-            p = e.GetPosition(ImageHolder);
-        }
-
-        private void ball_MouseMove(object sender, MouseEventArgs e)
-        {
-            Point x = e.GetPosition(ImageHolder);
-            if (e.LeftButton == MouseButtonState.Pressed)
-            {
-                Canvas.SetLeft(ball, Canvas.GetLeft(ball) + (x.X - p.X));
-                Canvas.SetTop(ball, Canvas.GetTop(ball) + (x.Y - p.Y));
-            }
-            p = x;
-        }
-
-        private void ball_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            timer.Start();
-            ball.ReleaseMouseCapture();
-        } */
-
         private void timer_Tick(object sender, EventArgs e)
         {
-            long ball1Pos = Convert.ToInt64(ball.GetValue(Canvas.TopProperty));
+            long ball1Pos = Convert.ToInt64(ball.GetValue(Canvas.TopProperty)); //olion ja pallon koordinaatit päivittyy tickin mukaan
             long ball2Pos = Convert.ToInt64(ball.GetValue(Canvas.LeftProperty));
             long slime1Pos = Convert.ToInt64(slime.GetValue(Canvas.TopProperty));
             long slime2Pos = Convert.ToInt64(slime.GetValue(Canvas.LeftProperty));
+            //pari omaa metodia, että koodi näyttää siistimmältä
             InsideTester(ball1Pos, ball2Pos);
             BallGravity(ball1Pos, ball2Pos);
             MoveSlime(slime2Pos);
             CollisionTester();
             txbPistetilanne.Text = "Happiness: " + points.ToString()  + "\nHunger: " + hunger.ToString();
         }
-        private void BallGravity(long ballTop, long ballLeft)
+        private void BallGravity(long ballTop, long ballLeft) //pallon "gravitaatio" eli pallo liikkuu y-suunnassa 8 aina kunnes osuu maahan
         {
             if (ballTop < 485)
             {
@@ -118,11 +94,11 @@ namespace Oliogotchi
             {
                 hunger += -2;
                 points += -5;
-                Canvas.SetLeft(ball, rnd.Next(10, 734));
+                Canvas.SetLeft(ball, rnd.Next(10, 734)); //uusi random x-koordinaatti aina kun pallo osuu maahan, mutta tietty korkeus
                 Canvas.SetTop(ball, 130);
             }
         }
-        private void slime_KeyDown(object sender, KeyEventArgs e)
+        private void slime_KeyDown(object sender, KeyEventArgs e) //olion liikkuminen A ja D napeilla
         {
             long slime2Pos = Convert.ToInt64(slime.GetValue(Canvas.LeftProperty));
             if (e.Key == Key.A)
@@ -136,7 +112,7 @@ namespace Oliogotchi
                 keyPressed = false;
             }
         }
-        private void MoveSlime(long slimeLeft)
+        private void MoveSlime(long slimeLeft) //olio automaattinen liikkumenen painettuun suuntaan
         {
             if (slimeLeft > 700)
             {
@@ -155,7 +131,7 @@ namespace Oliogotchi
                 Canvas.SetLeft(slime, slimeLeft + 7); ;
             }
         }
-        private void InsideTester(long ballTop, long ballLeft)
+        private void InsideTester(long ballTop, long ballLeft) //tarkistaa onko pallo ikkunan sisällä, jossei niin palaa tiettyyn pisteeseen
         {
             if (ballTop > 500 || ballLeft > 770 || ballLeft < -10)
             {
@@ -163,20 +139,20 @@ namespace Oliogotchi
                 Canvas.SetLeft(ball, 300);
             }
         }
-        private void CollisionTester()
+        private void CollisionTester() //törmäys testeri pallolle ja oliolle
         {
             var x1 = Canvas.GetLeft(ball);
             var y1 = Canvas.GetTop(ball);
-            Rect r1 = new Rect(x1, y1, ball.ActualWidth - 30, ball.ActualHeight - 30);
+            Rect r1 = new Rect(x1, y1, ball.ActualWidth - 25, ball.ActualHeight - 25);
 
 
             var x2 = Canvas.GetLeft(slime);
             var y2 = Canvas.GetTop(slime);
-            Rect r2 = new Rect(x2, y2, slime.ActualWidth - 30, slime.ActualHeight - 30);
+            Rect r2 = new Rect(x2, y2, slime.ActualWidth - 25, slime.ActualHeight - 25);
 
             if (r1.IntersectsWith(r2))
             {
-                points += 20;
+                points += 20; //jos osuu +20 happiness ja tietyt napit näkyville ja toistaa äänen
                 ball.Visibility = Visibility.Hidden;
                 timer.Stop();
                 btnAgain.Visibility = Visibility.Visible;
